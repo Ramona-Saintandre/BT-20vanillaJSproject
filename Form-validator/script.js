4d00666c9d545b6a6101e428979dd78a7c270583
Ramona Saintandre <codinggrandma@gmail.com>
2020-02-22 22:21:16 -0500

    fixed email validation

--- Form-validator/script.js
+++ Form-validator/script.js
@@ -5,51 +5,56 @@ const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
 // show input error message 
-function showError (input, message){
+function showError(input, message) {
   const formControl = input.parentElement;
   formControl.className = 'form-control error';
-  const small =formControl.querySelector('small');
+  const small = formControl.querySelector('small');
   small.innerText = message;
 }
 
-// Show success 
-function showSuccess(input, message){
+// Show success outline
+function showSuccess(input, message) {
   const formControl = input.parentElement;
   formControl.className = 'form-control success';
- }
+}
+
+//  Check if the email is valid 
+//https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+
+function isValidEmail(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
 
 // Eventlisteners 
-form.addEventListener('submit',function(e){
+form.addEventListener('submit', function (e) {
   e.preventDefault();
-    if(username.value === ''){
-    showError(username, "Username is required");
-    }else{
-      showSuccess(username);
-    }
-    if(username.value === ''){
-      showError(username, "Username is required");
-      }else{
-        showSuccess(username);
-      }
-      if(username.value === ''){
-        showError(username, "Username is required");
-        }else{
-          showSuccess(username);
-        }
-        if(username.value === ''){
-          showError(email, "Email is required");
-          }else{
-            showSuccess(email);
-          }
-          if(password.value === ''){
-            showError(password, "Password is required");
-            }else{
-              showSuccess(password);
-            }
-            if(password2.value === ''){
-              showError(password2, "Username is required");
-              }else{
-                showSuccess(password2);
-              }
 
-})
+  if (username.value === '') {
+    showError(username, "Username is required");
+  } else {
+    showSuccess(username);
+  }  
+
+  if (email.value === '') {
+    showError(email, "Email is required");
+
+  } else if (!isValidEmail(email.value)) {
+    showError(email, 'Email is not valid');
+  }else{
+    showSuccess(email);
+  }
+
+if (password.value === '') {
+  showError(password, "Password is required");
+} else {
+  showSuccess(password);
+};
+
+if (password2.value === '') {
+  showError(password2, "Username is required");
+} else {
+  showSuccess(password2);
+}
+
+});
